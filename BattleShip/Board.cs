4a3d0b38eaec8a70511f4cec7ec74d2458d01b3a
John Paul Depew <jpdepewjp@gmail.com>
2019-03-26 16:11:45 -0400

    Added some comments, etc

--- BattleShip/Board.cs
+++ BattleShip/Board.cs
@@ -42,6 +42,13 @@ namespace BattleShip
             return maxHits;
         }
 
+        /// <summary>
+        /// Sets a location to a certain string value. If the value is [X] then the total hits
+        /// for the board increases.
+        /// </summary>
+        /// <param name="x">x coordinate</param>
+        /// <param name="y">y coordinate</param>
+        /// <param name="setTo">string value to set the cell to</param>
         void SetLocation(int x, int y, string setTo)
         {
             if(setTo == "[X]")
@@ -62,6 +69,9 @@ namespace BattleShip
             }
         }
 
+        /// <summary>
+        /// Sets up all ships in shipLengths, checking to make sure it doesn't go off edge.
+        /// </summary>
         public void SetUpShips()
         {
             int shipIndex = 0;
@@ -120,6 +130,14 @@ namespace BattleShip
             Console.Clear();
         }
 
+        /// <summary>
+        /// Changes cells on table to [S] to represent a ship based on the orientation
+        /// </summary>
+        /// <param name="playerBoard"></param>
+        /// <param name="shipLength"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="orientation">The orientation of the ship (down or right)</param>
         void PlaceShip(string[,] playerBoard, int shipLength, int x, int y, string orientation)
         {
             for (int i = 0; i < shipLength; i++)
@@ -174,6 +192,11 @@ namespace BattleShip
             }
         }
 
+        /// <summary>
+        /// Asks the player for x and y coordinates and checks if it is a hit, miss, or if they already went there.
+        /// </summary>
+        /// <param name="enemyBoard"></param>
+        /// <returns></returns>
         public HitStatus Move(Board enemyBoard)
         {
             HitStatus hitStatus;
@@ -181,6 +204,8 @@ namespace BattleShip
             int x = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter Y coordinate: ");
             int y = Convert.ToInt32(Console.ReadLine());
+
+            // should add a check to make sure x and y are not out of bounds
 
             if (enemyBoard.GetLocation(x, y) == "[S]")
             {
